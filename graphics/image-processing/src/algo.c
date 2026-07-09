50e9d2d7b84758a7c46afc9d66bbe74b73370379
Marian <marianstefanduma@gmail.com>
2026-07-09 15:46:35 +0300

    feat(graphics/image-processing): implement edge thinning

--- graphics/image-processing/src/algo.c
+++ graphics/image-processing/src/algo.c
@@ -11,6 +11,12 @@
 #include <math.h>
 #include "algo.h"
 
+#ifndef M_PI
+#define M_PI 3.14159f // Good enough
+#endif
+
+#define M_RAD2DEG (180.0f/M_PI) // From raylib.h - skips the inclusion of the entire header
+#define M_DEG2RAD (M_PI/180.0f) // From raylib.h - skips the inclusion of the entire header
 
 static const float luminance_weights[] = { 0.299, 0.587, 0.114 };
 static const int lw_len = 3;
@@ -53,7 +59,8 @@ void* apply_red_filter(void* data, int width, int height, int bytes) {
     memcpy(out, ptr, bytes);
     for(size_t i = 0; i < bytes / px_size; i++) {
         // R: i * px_size + 0, G: i * px_size + 1, B: i * px_size + 2
-        out[i * px_size] = 0;
+        out[i * px_size + 1] = 0;
+        out[i * px_size + 2] = 0;
     }
 
     return out;
@@ -79,10 +86,7 @@ void* apply_binarization(void* data, int width, int height, int bytes) {
             
             for(int ch = 0; ch < lw_len; ch++) {
                 int index = (i * cols + j) * px_size + ch;
-                if(sum < threshhold)
-                    out[index] = 0;
-                else
-                    out[index] = 255;
+                out[index] = sum;
             }
         }
     }
@@ -130,8 +134,8 @@ void* apply_gaussian_filter(void* data, int width, int height, int bytes) {
 }
 
 void* apply_sobel_operator(void* data, int width, int height, int bytes) {
-    unsigned char* out_x = calloc(bytes, sizeof(unsigned char));
-    unsigned char* out_y = calloc(bytes, sizeof(unsigned char));
+    signed char* out_x = calloc(bytes, sizeof(signed char));
+    signed char* out_y = calloc(bytes, sizeof(signed char));
     unsigned char* out = calloc(bytes, sizeof(unsigned char));
     unsigned char* ptr = data;
     
@@ -186,13 +190,98 @@ void* apply_sobel_operator(void* data, int width, int height, int bytes) {
         }
     }
 
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
+
+    float theta = 0.0f;
+    float magnitude = 0.0f;
+    float negative = 0;
+    float positive = 0;
+    for(int i = 1; i < rows - 1; i++) {
+        for(int j = 1; j < cols - 1; j++) {
             for (int ch = 0; ch < px_size; ch++) {
                 int index = (i * cols + j) * px_size + ch;
                 int x = out_x[index] * out_x[index];
                 int y = out_y[index] * out_y[index];
-                out[index] = sqrtf(x + y);
+                
+                magnitude = sqrtf(x + y);
+                theta = M_RAD2DEG * atan2f(out_y[index], out_x[index]);
+                if(theta < 0) theta += 180.0f;
+                // rounded to 45
+                if(theta >= 45 - 22.5 && theta < 90 - 22.5) {
+                    int nw = ((i-1) * cols + j - 1) * px_size + ch;
+                    int se = ((i+1) * cols + j + 1) * px_size + ch;
+                    
+                    int xnw = out_x[nw] * out_x[nw];
+                    int ynw = out_y[nw] * out_y[nw];
+
+                    int xse = out_x[se] * out_x[se];
+                    int yse = out_y[se] * out_y[se];
+
+                    int magnw = sqrtf(xnw + ynw);
+                    int magse = sqrtf(xse + yse);
+
+                    if(magnitude > magnw && magnitude > magse)
+                        out[index] = magnitude;
+                    else
+                        out[index] = 0; 
+                }
+                // rounded to 90
+                else if(theta >= 90 - 22.5 && theta < 135 - 22.5) {
+                    int up = ((i-1) * cols + j) * px_size + ch;
+                    int down = ((i+1) * cols + j) * px_size + ch;
+
+                    int xu = out_x[up] * out_x[up];
+                    int yu = out_y[up] * out_y[up];
+
+                    int xd = out_x[down] * out_x[down];
+                    int yd = out_y[down] * out_y[down];
+
+                    int magu = sqrtf(xu + yu);
+                    int magd = sqrtf(xd + yd);
+
+                    if(magnitude > magu && magnitude > magd)
+                        out[index] = magnitude;
+                    else
+                        out[index] = 0; 
+                }
+                // rounded to 135
+                else if(theta >= 135 - 22.5 && theta < 180 - 22.5) {
+                    int ne = ((i-1) * cols + j + 1) * px_size + ch;
+                    int sw = ((i+1) * cols + j - 1) * px_size + ch;
+                    
+                    int xne = out_x[ne] * out_x[ne];
+                    int yne = out_y[ne] * out_y[ne];
+
+                    int xsw = out_x[sw] * out_x[sw];
+                    int ysw = out_y[sw] * out_y[sw];
+
+                    int magne = sqrtf(xne + yne);
+                    int magsw = sqrtf(xsw + ysw);
+
+                    if(magnitude > magne && magnitude > magsw)
+                        out[index] = magnitude;
+                    else
+                        out[index] = 0; 
+                }
+                // rounded to 0
+                else {
+                    int left = (i * cols + j - 1) * px_size + ch;
+                    int right = (i * cols + j + 1) * px_size + ch;
+
+                    int xl = out_x[left] * out_x[left];
+                    int yl = out_y[left] * out_y[left];
+
+                    int xr = out_x[right] * out_x[right];
+                    int yr = out_y[right] * out_y[right];
+
+                    int magl = sqrtf(xl + yl);
+                    int magr = sqrtf(xr + yr);
+
+                    if(magnitude > magl && magnitude > magr)
+                        out[index] = magnitude;
+                    else
+                        out[index] = 0; 
+                }
+                
             }
         }
     }
